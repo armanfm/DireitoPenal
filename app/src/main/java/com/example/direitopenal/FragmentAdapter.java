dbb6f4247c1405cbb5f2a01f29d8c1674f934d44
Armando José <armanfm@gmail.com>
2021-12-02 15:19:41 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/direitopenal/FragmentAdapter.java
@@ -0,0 +1,65 @@
+package adapter;
+
+import android.arch.lifecycle.Lifecycle;
+import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import com.example.direitopenal.CulpaFragment;
+import com.example.direitopenal.DireitoListFragment;
+import com.example.direitopenal.ExcludenteFragment;
+
+import java.util.List;
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+    DireitoListFragment dlf;
+
+    public FragmentAdapter(FragmentManager fm) {
+        super(fm);
+        dlf = new DireitoListFragment();
+
+    }
+
+
+    @Override
+    public Fragment getItem(int i) {
+
+
+        switch (i) {
+            case 1:
+                return new CulpaFragment();
+            case 2:
+                return new ExcludenteFragment();
+
+
+        }
+
+
+        return dlf;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 1:
+                return "Dolo e Culpa";
+            case 2:
+                return "Excludente do Dever Legal";
+        }
+
+        return "Tentativa e Consumação";
+    }
+
+
+}
+
+
