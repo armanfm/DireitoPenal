e319798d68cf99f4f27f62175edb521335954217
Armando José <armanfm@gmail.com>
2021-12-02 15:25:05 -0300

    Add files via upload

--- app/src/main/java/com/example/direitopenal/DireitoDetalheFragmento.java
+++ app/src/main/java/com/example/direitopenal/DireitoDetalheFragmento.java
@@ -1,5 +1,6 @@
 package com.example.direitopenal;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,13 +8,20 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,16 +30,26 @@ import com.google.android.youtube.player.YouTubePlayerView;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import adapter.AdapterDica;
+import classes.Direito;
+import helper.DbHelper;
+import helper.DireitoDAO;
+import helper.RecyclerItemClickListener;
 
 public class DireitoDetalheFragmento extends Fragment {
     public static final String TAG_DETALHE = "tagDetalhe";
     private  static final String EXTRA_DIREITO = "direito";
 
     TextView mTextAssunto;
-   // TextView mTextNome;
+    // TextView mTextNome;
     Button mTextConteudo;
-    Direito mDireito;
-
+    Direito mDireito = new Direito();
+    RecyclerView recyclerView;
+    List<Direito> listaDireito = new ArrayList<>();
+    Direito direito = new Direito();
 
 
 
@@ -49,45 +67,126 @@ public class DireitoDetalheFragmento extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         mDireito = (Direito) getArguments().getSerializable(EXTRA_DIREITO);
+        mDireito = (Direito) getArguments().getSerializable(EXTRA_DIREITO);
 
-         setHasOptionsMenu(true);
-         getActivity();
+        setHasOptionsMenu(true);
+        getActivity();
     }
 
-       @Override
+    @Override
     public View onCreateView( LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
-           mDireito= (Direito) getArguments().getSerializable(EXTRA_DIREITO);
+        mDireito= (Direito) getArguments().getSerializable(EXTRA_DIREITO);
 
-               View layout = inflater.inflate(R.layout.fragmento_detalhe_direito,container, false);
+     //   DbHelper db = new DbHelper(getContext().getApplicationContext());
+       // ContentValues cv = new ContentValues();
 
-                mTextAssunto =(TextView)layout.findViewById(R.id.txtAssunto);
-                mTextConteudo = (Button) layout.findViewById(R.id.txtConteudo);
+        //cv.put("dicas",direito.getDicas());
+        //db.getWritableDatabase().insert("DIREITO",null,cv);
 
-               if (mDireito != null){
-                   mTextAssunto.setText(mDireito.assunto);
-                   mTextConteudo.setOnClickListener(new View.OnClickListener() {
-                       @Override
-                       public void onClick(View v) {
-                           getUrl(mDireito.conteudo);
-                       }
-                   });
+        View layout = inflater.inflate(R.layout.fragmento_detalhe_direito,container, false);
+
+        mTextAssunto =(TextView)layout.findViewById(R.id.txtAssunto);
+        mTextConteudo = (Button) layout.findViewById(R.id.txtConteudo);
 
 
-               }
-               return layout;
+
+        //RECYCLEVIEW
+
+        //lista tarefa
+
+        EditText escreverDica=(EditText)layout.findViewById(R.id.txtEditar);
+        ImageButton adcDica =(ImageButton)layout.findViewById(R.id.imageButton);
+         DireitoDAO direitoDAO = new DireitoDAO(getContext().getApplicationContext());
+
+         listaDireito = direitoDAO.listar();
+         String dicas = escreverDica.getText().toString();
+         Direito direito1 = new Direito();
+
+
+
+
+
+            adcDica.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (!dicas.isEmpty())
+
+                        direito1.setDicas(dicas);
+
+                        direitoDAO.salvar(direito1);
+
+                    listaDireito.add(direito1);
+
+                }
+
+
+            });
+
+        //exibir lista de tarefas no reclycleview
+
+        //adapter
+        AdapterDica adapterDica = new AdapterDica(listaDireito);
+
+
+        recyclerView =layout.findViewById(R.id.carregarListaDicas);
+
+
+        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(
+                getContext().getApplicationContext(), recyclerView, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+
+            }
+
+            @Override
+            public void onLongItemClick(View view, int position) {
+
+            }
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        }
+        ));
+
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext().
+                getApplicationContext());
+         recyclerView.setLayoutManager(layoutManager);
+         recyclerView.setHasFixedSize(true);
+         recyclerView.addItemDecoration(new DividerItemDecoration(getContext().getApplicationContext(), LinearLayout.VERTICAL));
+         recyclerView.setAdapter(adapterDica);
+
+
+
+        if (mDireito != null){
+            mTextAssunto.setText(mDireito.getAssunto());
+            mTextConteudo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    getUrl(mDireito.getConteudo());
+                }
+            });
+
+
+        }
+        return layout;
 
     }
 
     private void getUrl(String conteudo) {
 
-            Uri url = Uri.parse(conteudo);
+        Uri url = Uri.parse(conteudo);
 
-            startActivity(new Intent(Intent.ACTION_VIEW, url));
+        startActivity(new Intent(Intent.ACTION_VIEW, url));
 
     }
 
 
 }
+
+
+
 
 
